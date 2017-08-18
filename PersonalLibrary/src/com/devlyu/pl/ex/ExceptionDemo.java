c95682421f6380bb2370b9d9a973d4c87d1ea23d
Forrest Lyu From MacOS <forrestlyu@163.com>
2017-08-18 21:34:46 +0800

    2017年08月18日21:34:44

---
+++ PersonalLibrary/src/com/devlyu/pl/ex/ExceptionDemo.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2017, Forrest Lyu. All rights reserved.
+ * 
+ */
+package com.devlyu.pl.ex;
+
+/**
+ * The {@code ExceptionDemo} class represents  
+ * 异常机制 
+ * @author Forrest Lyu, devlyu@163.com
+ * @version 1.0  , Aug 18, 2017
+ * 
+ */
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		Throwable
+	}
+}
